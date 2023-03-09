84536a14bd22edac4e53b42e1270f4604fa124b6
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-03-09 23:11:56 +0500

    Create a sign out feature

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,14 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+
+var alertMessage = document.getElementById("alert_message");
+var noticeMessage = document.getElementById("notice_message");
+
+setTimeout(function () {
+  noticeMessage.style.display = "none";
+}, 2000);
+
+setTime(function () {
+  alertMessage.style.display = "none";
+}, 2000);
