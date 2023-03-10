f07a95ff29d58637df11d13372fc0d27588cd89a
Khusniddin Ismoilov <husniddin.ismoilov@gmail.com>
2023-03-10 11:20:12 +0500

    Install gem cancancan and modify some files

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource param_method: :post_params, only: %i[create]
+
   def show
     @post = Post.includes(comments: :author).find_by(author_id: params[:user_id], id: params[:id])
     @comments = @post.comments.includes(:author)
@@ -22,6 +24,17 @@ class PostsController < ApplicationController
       redirect_to user_posts_path(current_user)
     else
       render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(author_id: params[:user_id], id: params[:post_id])
+    return render file: "#{Rails.root}/public/404.html", status: :not_found, layout: false if @post.nil?
+
+    if @post.destroy
+      redirect_to user_posts_path(current_user)
+    else
+      redirect_to user_post_path(current_user, @post)
     end
   end
 
